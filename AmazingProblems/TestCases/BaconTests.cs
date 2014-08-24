f71b55095dcd66a9ee18e5bdf4a72b08f42d66bc
Mike Randrup <github@mikerandrup.com>
2014-08-24 13:12:52 -0500

    Basic project setup

---
+++ AmazingProblems/TestCases/BaconTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestCases
+{
+    [TestClass]
+    public class BaconTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ AmazingProblems/TestCases/ChemSpellTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestCases
+{
+    [TestClass]
+    public class ChemSpellTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
