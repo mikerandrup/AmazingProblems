e4c620a05fa713e79391e24c5a0a764103b5c81f
Mike Randrup <github@mikerandrup.com>
2014-08-25 19:10:44 -0500

    Cleaned up using statements in DegreesOfBacon

--- Problems/DegreesOfBacon/DataProvider/Categories.cs
+++ Problems/DegreesOfBacon/DataProvider/Categories.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Problems.DegreesOfBacon.DataProvider
+﻿namespace Problems.DegreesOfBacon.DataProvider
 {
     public enum Categories
     {
