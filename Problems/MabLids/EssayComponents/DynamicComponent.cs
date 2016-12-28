e032b29d86f6139cf8ac4895f02a0183e3a55241
Mike Randrup <github@mikerandrup.com>
2016-12-27 21:18:17 -0600

    Re-created missing class that never was added to repo

---
+++ Problems/MabLids/EssayComponents/DynamicComponent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems.MabLids.EssayComponents
+{
+    public class DynamicComponent : EssayComponent
+    {
+        public DynamicComponent SetValue(string value)
+        {
+            _value = value;
+            return this;
+        }
+        private string _value;
+
+        public override string ToString()
+        {
+            return _value;
+        }
+    }
+}
